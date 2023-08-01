488274bde923dd8987aae074fe29aefb83afd6fb
dgsuarez76 <141162104+dgsuarez76@users.noreply.github.com>
2023-08-01 15:23:21 -0400

    Update DemoDay2.java

--- src/day2/DemoDay2.java
+++ src/day2/DemoDay2.java
@@ -20,3 +20,8 @@ public static void subtract ( int a, int b) {
 	public static void divide( int a, int b) {
 	System.out.println (a / b);
 }
+//Yasir
+	public static void add( int a, int b, int c) {
+	System.out.println (a + b + c);	
+}
+}
